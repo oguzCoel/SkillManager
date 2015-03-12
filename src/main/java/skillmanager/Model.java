0761c23e908bf8c01bb99b20fe2119087f131a7b
oguzCoel <oguzcoel@gmail.com>
2015-03-12 21:19:09 +0100

    take

--- src/main/java/skillmanager/Model.java
+++ src/main/java/skillmanager/Model.java
@@ -5,27 +5,67 @@ import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 
 import org.openrdf.model.Value;
+import org.openrdf.query.BindingSet;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.http.HTTPRepository;
 
-@ManagedBean
 public class Model {
-	private ArrayList<Value> list;
-	
-	public Model(){
+
+	public static void main(String[] args) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
 		
-		list = new ArrayList<Value>();
+	
+	ArrayList<String> list = new ArrayList<String>();
+	String sesameServer = "http://localhost:8080/openrdf-sesame";
+	String repositoryId = "uni";
+	
+
+		
+		
 	
 		//Verbindung mit Repository erstellen
-		//RepositoryConnection con = repo.getConnection();
+		Repository repo = new HTTPRepository(sesameServer, repositoryId);
+		repo.initialize();
 		
+		RepositoryConnection con = repo.getConnection();
+		String queryString = "PREFIX AB: <http://people.brunel.ac.uk/~csstnns/university.owl#> SELECT ?z WHERE {AB:Lecturer1 AB:teaches ?z}";
+		TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
 		
-			//Query definieren
-			String queryString = "SELECT * {?x ?y ?z}";
-		//	TupleQuery tupleQuery = con.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+		TupleQueryResult result = tupleQuery.evaluate();
+		
+		while (result.hasNext()){
+			BindingSet bindingSet = result.next();
+			//Value valueOfX = bindingSet.getValue("x");
+			//Value valueOfY = bindingSet.getValue("y");
+			Value valueOfZ = bindingSet.getValue("z");
 			
+			if (valueOfZ.toString().contains("#")){
+			
+			String [] rec = valueOfZ.stringValue().split("#");
+			String a = rec[0];
+			String b = rec[1];
+			list.add(b);
+			
+			
+			}
+		}
+		
+		for (String s: list){
+			System.out.println(s);
+		}
+		
 	}
+			
+}
+	
+	
+		
 	
 
-}
+
