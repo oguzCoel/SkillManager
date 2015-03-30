977f77af01ec2c272d2df3586350f37ba4b00afe
oguzCoel <oguzcoel@gmail.com>
2015-03-30 23:29:55 +0200

    Delete all relatioships to sesame

--- src/main/java/ch/quickline/business/dao/PersistenceType.java
+++
@@ -1,6 +0,0 @@
-package ch.quickline.business.dao;
-
-public enum PersistenceType {
-	SESAME, MYSQL;
-
-}
--- src/main/java/ch/quickline/business/dao/StudentDao.java
+++
@@ -1,9 +0,0 @@
-package ch.quickline.business.dao;
-
-public interface StudentDao {
-	public void insertStudent(String staffID, String first_name, String last_name);
-	public void deleteStudent();
-	public void findStudent();
-	
-
-}
