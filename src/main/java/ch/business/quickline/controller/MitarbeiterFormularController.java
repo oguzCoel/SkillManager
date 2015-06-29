20415721b8e3dc0e662e1c7064ee9d56d1cb956c
Antonino Di Giovanna <antonino@vtxmail.ch>
2015-06-29 21:52:41 +0200

    Loggin Exception in Klasse MitarbeiterFormularController #32

--- src/main/java/ch/business/quickline/controller/MitarbeiterFormularController.java
+++ src/main/java/ch/business/quickline/controller/MitarbeiterFormularController.java
@@ -8,6 +8,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -27,6 +28,8 @@ import ch.business.quickline.service.RoleService;
 @Component
 @Scope("request")
 public class MitarbeiterFormularController implements Serializable {
+	//Log
+	final Logger logger = Logger.getLogger(MitarbeiterFormularController.class);
 	
 	@Autowired
 	private MitarbeiterService mitarbeiterService;
@@ -142,18 +145,24 @@ public class MitarbeiterFormularController implements Serializable {
 
 	
 	public void save()throws Exception{
-		mitarbeiterService.save(mitarbeiter);
-		benutzer.setMitarbeiter(mitarbeiter);
-		benutzerService.save(benutzer);
-		benutzerRole.setBenutzer(benutzer);
-		benutzerRoleService.save(benutzerRole);
-		unternehmenViewController.init();
-		mitarbeiterTable.init();
 		
-		FacesMessage msg = new FacesMessage
-				(getMitarbeiter().getMitarbeiterVorname() + " " +
-		getMitarbeiter().getMitarbeiterNachname() + " erfolgreich registriert");
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+		try {
+			mitarbeiterService.save(mitarbeiter);
+			benutzer.setMitarbeiter(mitarbeiter);
+			benutzerService.save(benutzer);
+			benutzerRole.setBenutzer(benutzer);
+			benutzerRoleService.save(benutzerRole);
+			unternehmenViewController.init();
+			mitarbeiterTable.init();
+			FacesMessage msg = new FacesMessage(getMitarbeiter()
+					.getMitarbeiterVorname()
+					+ " "
+					+ getMitarbeiter().getMitarbeiterNachname()
+					+ " erfolgreich registriert");
+			FacesContext.getCurrentInstance().addMessage(null, msg);
+		} catch (Exception e) {
+			logger.error("Fehler beim Abspeichern Mitarbeiter. Details" +e.getMessage());
+		}
 		
 			
 		
