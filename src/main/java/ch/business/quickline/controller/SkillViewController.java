517cfe899ecc7740ee14b32669573e691f39d0fa
oguzCoel <oguzcoel@gmail.com>
2015-07-03 21:59:32 +0200

    xhtml Seiten anpassen #40

--- src/main/java/ch/business/quickline/controller/SkillViewController.java
+++ src/main/java/ch/business/quickline/controller/SkillViewController.java
@@ -1,20 +1,26 @@
 package ch.business.quickline.controller;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
+import org.primefaces.event.ToggleEvent;
 import org.primefaces.model.chart.MeterGaugeChartModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import ch.business.quickline.domain.MitarbeiterSkill;
 import ch.business.quickline.domain.Skill;
+import ch.business.quickline.domain.SkillMaster;
 import ch.business.quickline.service.MitarbeiterSkillService;
+import ch.business.quickline.service.SkillMasterService;
 import ch.business.quickline.service.SkillService;
 
 @ManagedBean
@@ -28,6 +34,9 @@ public class SkillViewController {
 	@Autowired
 	private MitarbeiterSkillService mitarbeiterSkillService;
 	
+	@Autowired
+	private SkillMasterService skillMasterService;
+	
 	 private MeterGaugeChartModel masterBewertungenSkill;
 	 private MeterGaugeChartModel selbstBewertungenSkill;
 	 private Skill skill;
@@ -36,6 +45,11 @@ public class SkillViewController {
 	 private List<MitarbeiterSkill> selbstBewertungRanking;
 	 private Double masterBewertungDurchschnitt;
 	 private Double selbstBewertungDurchschnitt;
+	 private String masterBewertungDurchschnittString;
+	 private String selbstBewertungDurchschnittString;
+	 private SkillMaster skillMaster;
+	 
+	 private DecimalFormat df = new DecimalFormat("#.###");
 	 
 	 
 	 @ManagedProperty("#{param.id}")
@@ -56,6 +70,9 @@ public class SkillViewController {
 		    selbstBewertungRanking = mitarbeiterSkillService.findBySkillOrderBySelbstBewertungDesc(skill);
 		    masterBewertungDurchschnitt = skillService.retrieveMasterBewertungDurchschnitt(skill.getSkillId());
 		    selbstBewertungDurchschnitt = skillService.retrieveSelbstBewertungDurchschnitt(skill.getSkillId());
+		    masterBewertungDurchschnittString = df.format(skillService.retrieveMasterBewertungDurchschnitt(skill.getSkillId()));
+			selbstBewertungDurchschnittString = df.format(skillService.retrieveSelbstBewertungDurchschnitt(skill.getSkillId()));
+			skillMaster = skillMasterService.findBySkill(skill);
 		
 	 }
 	 
@@ -85,12 +102,12 @@ public class SkillViewController {
 	    public void createMeterGaugeModels() {    
 	        
 	        masterBewertungenSkill = initMeterGaugeModel();
-	        //masterBewertungenGlobal.setTitle("Masterbewertungen");
+	        masterBewertungenSkill.setTitle("Masterbewertungen");
 	        masterBewertungenSkill.setGaugeLabel("Skill");
 	        masterBewertungenSkill.setValue(masterBewertungDurchschnitt);
 	        
 	        selbstBewertungenSkill = initMeterGaugeModel();
-	        //selbstBewertungenGlobal.setTitle("Selbstbewertungen");
+	        selbstBewertungenSkill.setTitle("Selbstbewertungen");
 	        selbstBewertungenSkill.setGaugeLabel("Interesse");
 	        selbstBewertungenSkill.setValue(selbstBewertungDurchschnitt);
 	    }
@@ -146,15 +163,37 @@ public class SkillViewController {
 		public void setId(Integer id) {
 			this.id = id;
 		}
-	        
+
+		public String getMasterBewertungDurchschnittString() {
+			return masterBewertungDurchschnittString;
+		}
+
+		public void setMasterBewertungDurchschnittString(
+				String masterBewertungDurchschnittString) {
+			this.masterBewertungDurchschnittString = masterBewertungDurchschnittString;
+		}
+
+		public String getSelbstBewertungDurchschnittString() {
+			return selbstBewertungDurchschnittString;
+		}
+
+		public void setSelbstBewertungDurchschnittString(
+				String selbstBewertungDurchschnittString) {
+			this.selbstBewertungDurchschnittString = selbstBewertungDurchschnittString;
+		}
+
+		public SkillMaster getSkillMaster() {
+			return skillMaster;
+		}
+
+		public void setSkillMaster(SkillMaster skillMaster) {
+			this.skillMaster = skillMaster;
+		}
 		
-		
-		
-		
-		
-		
-	        
-	    
-	        
+		 public void handleToggle(ToggleEvent event) {
+		        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Toggled", "Visibility:" + event.getVisibility());
+		        FacesContext.getCurrentInstance().addMessage(null, msg);
+		    }
+
 
 }
